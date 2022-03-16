c981bb95e81c4266fd506c545fa73dccb35432bf
diegopalacioscr <54324135+diegopalacioscr@users.noreply.github.com>
2022-03-15 18:47:46 -0600

    Actualización User
    Se une la carpeta User donde se maneja de forma aislada la parte del usuario normal

---
+++ user/index.php
@@ -0,0 +1,92 @@
+<?php
+	/**************************************************
+		Sistema de administracion
+		Desarrollador: Gilberth Monge
+		Año de creación: 2020
+		Última modificación del archivo: 21-04-2020
+	**************************************************/
+	/** Inicializaciones */
+		@session_start();
+		include_once('../core/variables_globales.php');
+		include_once('../core/quick_function.php');
+		$Quick_function = new Quick_function;
+    /** Inicializaciones */
+    
+	/** Verifica si esta logueado * /
+        $eslogueado=$Quick_function->es_logueado();
+		if($eslogueado!=true){ header('Location: ../'); }
+	/** Verifica si esta logueado */
+    
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <?php include_once("template/head.php") ?>
+    </head>
+
+    <body>
+        <?php include_once("template/header.php") ?>
+
+        <section class="slice pt-5 margin-header bg-section-secondary" style="padding-bottom: 0rem;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 px-2">
+                        <h2>Bienvenido<h2>
+                        <h2>Nombre de Usuario<h2>    
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="slice bg-section-secondary">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-4 px-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <a href="#" class="d-block h5 mt-3">Informaci&oacute;n</a>
+                                <div class="row align-items-center mt-4">
+                                    <div class="col-12">
+                                        <a href="info.php" class="btn btn-sm btn-block btn-primary btn-primary-dashboard">Ver informaci&oacute;n</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 px-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <a href="#" class="d-block h5 mt-3">Editar Informaci&oacute;n</a>
+                                <div class="row align-items-center mt-4">
+                                    <div class="col-12">
+                                        <a href="user-edit.php" class="btn btn-sm btn-block btn-primary btn-primary-dashboard">Actualizar Informaci&oacute;n</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 px-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <a href="#" class="d-block h5 mt-3">Cursos Disponibles</a>
+                                <div class="row align-items-center mt-4">
+                                    <div class="col-12">
+                                        <a href="cursos.php" class="btn btn-sm btn-block btn-primary btn-primary-dashboard">Ir al módulo</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <?php include_once("template/libs.php") ?>
+        <?php include_once("template/footer.php") ?>
+    </body>
+
+</html>
---
+++ user/template/libs.php
@@ -0,0 +1,45 @@
+<?php 
+	/**************************************************
+		Sistema de administracion
+		Desarrollador: Gilberth Monge
+		Año de creación: 2020
+		Última modificación del archivo: 21-04-2020
+	**************************************************/
+    /* contiene las libs js general del sistema */
+?>
+
+        <!-- Core JS  -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/0265b153d4.js" ></script>
+        
+        <!-- Quick JS -->
+        <script src="../assets/libs/quick-website/js/quick-website.js"></script>
+
+        <!-- Bootstrap select -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+
+        <!-- Data Tables -->
+        <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
+        <!-- Bootstrap date time picker -->
+        <script src="../assets/libs/bootstrap-datetimepicker/js/bootstrap-datepicker.min.js"></script>
+        <script src="../assets/libs/bootstrap-datetimepicker/js/bootstrap-datepicker.es.min.js"></script>
+
+        <!-- Formvalidation -->
+        <script src="../assets/libs/formvalidation-v1.5.0/dist/js/FormValidation.min.js"></script>
+        <script src="../assets/libs/formvalidation-v1.5.0/dist/js/plugins/Bootstrap.min.js"></script>
+        
+        <!-- Rexy Studio JS -->
+        <script src="../assets/expediente/js/script.js"></script>
+        <script src="../assets/expediente/js/validaciones.js"></script>
+        
+        <script>
+            $(document).ready(function () {
+                obtener_frase("")
+            })
+        </script>
